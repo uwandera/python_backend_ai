94bb606145f0a3898b8eceb237946cb5ca44936d
uwandera <tomuwande@gmail.com>
2024-05-03 02:12:36 -0300

    commit final do modulo fundamental de python

--- Fundamentos/string_2.py
+++ Fundamentos/string_2.py
@@ -1,10 +1,10 @@
-nome = "Guilherme"
+nome = "Thomas "
 idade = 28
 profissao = "Progamador"
 linguagem = "Python"
 saldo = 45.435
 
-dados = {"nome": "Guilherme", "idade": 28}
+dados = {"nome": "Thomas", "idade": 28}
 print(dados)
 
 # %s para substituir string , %d para substituir por inteiro, %f substitui por ponto flutuante
@@ -12,7 +12,7 @@ print("Nome: %s Idade: %d" % (nome, idade))# a ordem das variaveis tem que ser o
 
 #formatação de string usando abre e fecha chaves
 #se trocar as variaveis de posição a resposta fica incoerente
-print("Nome: {} Idade: {}".format(idade, nome))# a ordem das variasveis dentro do .format(var1,var2)
+print("Nome: {} Idade: {}".format(nome, idade))# a ordem das variasveis dentro do .format(var1,var2)
 
 # pode usar numeros(posição) da variavel que queremos colocar na string
 #da pra virar uma bagunça se não cuidar
@@ -24,8 +24,12 @@ print("Nome: {1} Idade: {1} Nome: {1} {0} {1}".format(idade, nome))
 print("Nome: {nome} Idade: {idade}".format(nome=nome, idade=idade))
 #podemos trocar o nome da variavel dentro das chaves mas temos que fazer uma operação de varNEW recebe varOLD
 print("Nome: {name} Idade: {age} {name} {name} {age}".format(age=idade, name=nome))
+#uso de dicionario para efetuação da formatação
 print("Nome: {nome} Idade: {idade}".format(**dados))
 
+#melhor forma de formatar com as variaveis
 print(f"Nome: {nome} Idade: {idade}")
-print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:.2f}")
-print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:10.1f}")
+
+# width.nf onde width é um numero .n é um numero e o f é float
+print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:1.2f}") # sem espaços a esquerda e com duas casas decimais
+print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:1.1f}") #10 casas de espaço e 1 casa decimal
---
+++ Fundamentos/string_3.py
@@ -0,0 +1,10 @@
+nome = "Thomas Uwande Alves Cardoso"
+#podemos olhar strings como vetores [start:stop:step]
+print(nome[0])#indice é usado para 
+print(nome[-2])# numeros negativos são usados para caminha do final para o começõ
+print(nome[:9])#começa da posição [0] e vai até a [9], neste caso o ultimo caracter nao sera incluido
+print(nome[10:])# apartir do start até o final da string
+print(nome[10:16])#sub string start e stop, ao passo 1 a 1
+print(nome[10:16:2])# sub string star:stop:step
+print(nome[:])# copia da string inteira
+print(nome[::-1]) #espelhamento de string, exibir de tras pra frente
