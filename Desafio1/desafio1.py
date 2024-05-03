10d283fe9171e2e6c114d37855432dc3c733fe67
uwandera <tomuwande@gmail.com>
2024-05-03 15:30:32 -0300

    módulo saque v0.99

--- Desafio1/desafio1.py
+++ Desafio1/desafio1.py
@@ -5,12 +5,11 @@ SAQUE --> [S]
 DEPOSITO --> [D]
 EXTRATO --> [E]
 SAIR --> [Q]
->>> 
-'''
-saldo = 0
+>>> '''
+
+saldo = 1
 extrato = ""
-numero_saques = 0
-LIMITE_SAQUE_DIARIO = 3
+Limite_Saque_Diario = 3
 LIMITE_VALOR_POR_SAQUE = 500
 
 
@@ -21,17 +20,41 @@ while True:
         valor = float(input("Qual o valor que deseja depositar: "))
         #verificação para garantir que valores não são negativos
         if valor > 0: 
-            saldo =+ valor
-            print(f"Seu saldo é de R${saldo:2.2f}")
+            #atentar a ordem do +=
+            saldo += valor
+            print(f"Seu saldo é de R$ {saldo:2.2f}")
             #OPERAÇÂO DE GUARDAR O DEPOSITO NO EXTRATO
             extrato = (f"DEPÒSITO: R$ {valor:.2f}")
+            
         
         else:
             print("valor inválido, tente novamente!")
-            break
 
-        
-        
-    
+    elif opcao == "s":         
+            while Limite_Saque_Diario != 0:
+                if saldo > 0:
+                    print(f"Seu saldo é de R${saldo:2.2f}")
+                    saque = float(input("qual valor deseja sacar: "))
+                    if saque > 500:
+                        print(f"seu saque limite é R$ {LIMITE_VALOR_POR_SAQUE:3.2f}")
+                    elif saque > saldo:
+                        print("valor desejado insuficiente no momento, tente um outro valor")
+                    else:
+                        #a ordem do "-="-->(saldo = saldo - saque) ou "=-" -->(saldo = -saque) muda totalmente a conta que está sendo feita, usar saldo -= 
+                        saldo -= saque
+                        print(f"Seu saldo é de R$ {saldo:2.2f}")
+                continuar = input(f"deseja fazer mais um saque, seu limite diario é de {Limite_Saque_Diario} (S ou N):  ")
+                if continuar == "s":
+                    Limite_Saque_Diario -= 1
+                elif continuar == "n":
+                    break
 
+    #elif opcao == "e":
+
+
+            
+                
+            
+   
+        
 quit()
