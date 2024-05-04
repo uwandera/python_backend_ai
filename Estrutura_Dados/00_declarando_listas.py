35157eb25f3c619f08f3750c00e2e796b4c41032
uwandera <tomuwande@gmail.com>
2024-05-04 01:04:39 -0300

    declaração de listas

---
+++ Estrutura_Dados/00_declarando_listas.py
@@ -0,0 +1,20 @@
+
+#lista pode ser tratada como um tipo de vetor
+frutas = ["laranja", "maçã", "uva","banana","carambola","manga"]
+print(frutas)
+
+#listas podem ser vazias
+frutas = []
+print(frutas)
+
+#podemos guardar valores em variaveis de forma transformando-os em vetores 
+letras = list("python")
+print(letras)
+
+#criar vetores sequenciais usando range
+numeros = list(range(10))
+print(numeros)
+
+#criar vetores de forma mista
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
