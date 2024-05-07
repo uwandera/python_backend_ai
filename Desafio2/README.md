3d36a0ef6c9fd21b3eb6fd419caa170767a34177
uwandera <tomuwande@gmail.com>
2024-05-07 17:34:19 -0300

    branch nova só para atualizar sobre o desafio 02

---
+++ Desafio2/README.md
@@ -0,0 +1,27 @@
+# Desafio 1
+## Desenvolver o novo sistema
+    Linguagem: Python 3
+    versão: 1
+    objetivo: implementar apenas 3 operações --> depósito;
+                                             --> sáque;
+                                             --> extrato; 
+    
+## Operação de DEPÓSITO
+ ⁂depositar valores positivos
+ ⁂versão 1 só trabalha com 1 usuario
+ ⁂numero da agência e conta são opcionais
+ ⁂todos os depositos devem ser armazenados em uma variavel
+ ⁂todos os depositos devem ser exibidos em EXTRATO
+
+## operação de SAQUE
+    ⁂permitir realizar 3 saques diários
+    ⁂limite por saque é de R$500
+    ⁂ caso não haja saldo, informar cliente
+    ⁂Todos os saques devem ser armazenados emuma variavel
+    ⁂Exibir todos os saques na operação extrato
+
+## operação de EXTRATO
+    ⁂listar todos os depositos da conta
+    ⁂listar todos os saques da conta
+    ⁂exibir saldo atual da conta
+    ⁂usar formato de R$ xxxx.xx
---
+++ Desafio2/desafio2.py
@@ -0,0 +1,65 @@
+menu = f'''######  QUAL OPERAÇÂO VOCÊ DESEJA EFETUAR   ######
+SAQUE --> [S]
+DEPOSITO --> [D]
+EXTRATO --> [E]
+SAIR --> [Q]
+>>> '''
+
+saldo = 1
+extrato = ("")
+Limite_Saque_Diario = 3
+LIMITE_VALOR_POR_SAQUE = 500
+
+
+while True:
+    opcao = input(menu)
+    if opcao == "d":
+        # OPERAÇÂO DEPÒSITO
+        valor = float(input("Qual o valor que deseja depositar: "))
+        #verificação para garantir que valores não são negativos
+        if valor > 0: 
+            #atentar a ordem do +=
+            saldo += valor
+            print(f"Seu saldo é de R$ {saldo:2.2f}")
+            #OPERAÇÂO DE GUARDAR O DEPOSITO NO EXTRATO
+            extrato += (f"DEPÒSITO: R$ {valor:.2f} "+"\n")
+            
+        
+        else:
+            print("valor inválido, tente novamente!")
+
+    elif opcao == "s":         
+            while Limite_Saque_Diario != 0:
+                if saldo > 0:
+                    print(f"Seu saldo é de R${saldo:2.2f}")
+                    saque = float(input("qual valor deseja sacar: "))
+                    if saque > 500:
+                        print(f"seu saque limite é R$ {LIMITE_VALOR_POR_SAQUE:3.2f}")
+                    elif saque > saldo:
+                        print("valor desejado insuficiente no momento, tente um outro valor")
+                    else:
+                        #a ordem do "-="-->(saldo = saldo - saque) ou "=-" -->(saldo = -saque) muda totalmente a conta que está sendo feita, usar saldo -= 
+                        saldo -= saque
+                        print(f"Seu saldo é de R$ {saldo:2.2f}")
+                        extrato += (f"SAQUE: R$ {saque:.2f} "+"\n")
+                continuar = input(f"deseja fazer mais um saque, seu limite diario é de {Limite_Saque_Diario - 1} (S ou N):  ")
+                if continuar == "s":
+                    Limite_Saque_Diario -= 1
+                elif continuar == "n":
+                    break
+                else:
+                    print("este valor não é válido, tente novamente")
+                    break
+
+    elif opcao == "q":
+        break
+
+    elif opcao == "e":
+        print(extrato)
+    
+    else:
+        break
+
+   
+      
+quit()
